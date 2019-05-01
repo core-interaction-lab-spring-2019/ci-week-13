2bed69b7460fbc8945fed0448112e600a72095ba
jrrrsmith <jeremy.scott.smith@me.com>
2019-05-01 19:02:39 -0400

    changed layout for forecast

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ window.addEventListener('load', ()=> {
 	let long;
 	let lat;
 
-	let description = document.querySelector('.temp__description');
-	let degrees = document.querySelector('.temp__value');
+	let description = document.querySelector('.current-description');
+	let degrees = document.querySelector('.current-temp');
 	let timezone = document.querySelector('.location__timezone');
 	let body = document.querySelector('body');
-
+	
 	if(navigator.geolocation) {
 
 		navigator.geolocation.getCurrentPosition(position => {
@@ -18,7 +18,6 @@ window.addEventListener('load', ()=> {
 			const proxy = 'http://cors-anywhere.herokuapp.com/';
 			const api = `${proxy}https://api.darksky.net/forecast/b37667a21bdc871f0bbf54ca09f6cbfc/${lat},${long}`;
 
-
 			fetch(api)
 				.then(response => {
 					return response.json();
@@ -27,29 +26,20 @@ window.addEventListener('load', ()=> {
 					console.log(data);
 
 					const { temperature, summary, icon } = data.currently;
+					const timezone_location = data.timezone;
+					const timezone_edit = timezone_location.replace(/_/g, " ");
 
 					//set DOM element values w/ values from the API
 
 					description.textContent = summary;
 					degrees.textContent = temperature;
-					timezone.textContent = data.timezone;
+					timezone.textContent = timezone_edit;
 
 					body.classList.add(icon);
-
-					setIcons(icon, document.querySelector('.icon'));
 
 				});
 
 		});
 
-	}
-
-	function setIcons(icon, iconID) {
-
-		const skycons = new Skycons({color: "white"});
-		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-		skycons.play();
-
-		return skycons.set(iconID, Skycons[currentIcon]);
 	}
 });
